98df983f70e53a34e5e43ad3a05dbd0b25108e4d
JDoosk <minimus713@gmail.com>
2024-02-22 19:30:33 -0800

    Added dayjs, added default date value set to today. Added a select to choose what you are making.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,55 @@
+'use client';
+
+import { ChangeEvent, useState } from 'react';
+import dayjs from 'dayjs';
+
 export default function Page() {
+  const [brewStartDate, setBrewStartDate] = useState(
+    dayjs().format('YYYY-MM-DD')
+  );
+  const [brewType, setBrewType] = useState('');
+
+  const changeDate = (value: ChangeEvent<HTMLInputElement>) => {
+    setBrewStartDate(value.target.value);
+  };
+
+  const changeBrew = (value: ChangeEvent<HTMLSelectElement>) => {
+    console.log(value.target.value);
+    setBrewType(value.target.value);
+  };
+
+  const submit = () => {
+    console.log(brewStartDate, brewType);
+    console.log(dayjs().format('YYYY-MM-DD'));
+
+    if (!brewType) console.log('Nah bruh, go fix yo sheesh');
+  };
+
   return (
-    <div>
-      <form>
-        <input
-          type="date"
-          id="brew_date"
-        />
-        <button type="submit">Enter</button>
-      </form>
+    <div className="m-4 color-red">
+      <p>Starting Brew Date</p>
+      <input
+        type="date"
+        id="brewdate"
+        name="date"
+        defaultValue={brewStartDate}
+        onChange={changeDate}
+      />
+      <br />
+      <button
+        type="button"
+        onClick={submit}
+      >
+        Enter
+      </button>
+
+      <select onChange={changeBrew}>
+        <option value="">-- Whatcha Brewin'? -- </option>
+        <option value="mead">Mead</option>
+        <option value="cyser">Cyser</option>
+        <option value="cider">Cider</option>
+        <option value="beer">Beer</option>
+      </select>
     </div>
   );
 }
